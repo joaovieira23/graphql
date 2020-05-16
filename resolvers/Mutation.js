362476923fdc21ffedf0f0748200f071dc2142be
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-16 14:54:36 -0300

    feat: set mutation to change user

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -28,5 +28,29 @@ module.exports = {
     
     const excluidos = usuarios.splice(i, 1)
     return excluidos ? excluidos[0] : null
+  },
+
+  alterarUsuario(_, args) {
+    const i = usuarios
+      .findIndex(u => u.id == args.id)
+    
+      if(i < 0) return null
+
+      usuarios[i].nome = args.nome
+      usuarios[i].email = args.email
+      
+      if(args.idade) {
+        usuarios[i].idade = args.idade
+      }
+
+      // const usuario = {
+      //   ...usuarios[i],
+      //   ...args
+      // }
+
+      // usuarios.splice(i, 1, usuario)
+      // return usuario
+      
+      return usuarios[i]
   }
 }
