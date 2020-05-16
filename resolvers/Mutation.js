ecf48f4c8f53861f353ea085acdd8d56909e7a36
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-16 14:29:42 -0300

    feat: set mutation to delete user

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -19,5 +19,14 @@ module.exports = {
 
     usuarios.push(novo)
     return novo
+  },
+
+  excluirUsuario(_, { id }) {
+    const i = usuarios.findIndex(u => u.id === id)
+
+    if (i < 0) return null
+    
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
   }
 }
